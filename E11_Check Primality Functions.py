184e9652b7cb2e1348661df4f01057bb1168da23
rotau <ronitau@gmail.com>
2022-07-19 16:03:53 +0300

    E11_Check Primality Functions.py

--- E11_Check Primality Functions.py
+++ E11_Check Primality Functions.py
@@ -18,5 +18,3 @@ if calculate_prime(selected_number):
     print(f'{selected_number}  is a prime number')
 else:
     print(f'{selected_number} is not a prime number')
-print('this is a new screen')
-print('new button')
