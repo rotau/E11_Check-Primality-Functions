99130853f343337428a8afe7e1926e47c44c9055
rotau <ronitau@gmail.com>
2022-07-19 14:35:20 +0300

    first commit

---
+++ E11_Check Primality Functions.py
@@ -0,0 +1,20 @@
+def get_number():
+    return int(input('please select a number: '))
+
+
+def calculate_prime(selected_number):
+    prime = False
+    for number in range(2, selected_number):
+        if selected_number % number == 0:
+            prime = False
+            break
+        else:
+            prime = True
+    return prime
+
+
+selected_number = get_number()
+if calculate_prime(selected_number):
+    print(f'{selected_number}  is a prime number')
+else:
+    print(f'{selected_number} is not a prime number')
